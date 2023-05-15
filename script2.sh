c945db224130b43792f7c9bb2b6fbcbc077007aa
unknown <varains2230_nisc@outlook.com>
2023-05-15 21:49:12 +0530

    script2

---
+++ script2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###### Script to add 2 numbers with the entries in the file itself ########
+echo "+++++++++++++ Program to add 2 numbers +++++++++++++++"
+echo 
+a=35
+b=42
+c=`expr $a + $b`
+echo "===============Value of c:$c"
