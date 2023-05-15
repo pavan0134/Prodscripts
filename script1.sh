93911674bc1d6c1c30d79f552933ddae71297073
unknown <varains2230_nisc@outlook.com>
2023-05-15 21:48:16 +0530

    script1

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "------------------My First Script---------------------"
+echo
+echo " -------------- Server Date ---------------"
+date 
+echo "------------ Server Uptime ----------------------"
+uptime
+echo "------------- Server Information -----------------"
+uname -a
+echo
